00c77a09d36f9d24c6b8619fab323e38eae728e5
Mongoose <smn9008@outlook.com>
2023-11-18 19:04:16 +0800

    v1.0.09

--- Software/auto_test.py
+++ Software/auto_test.py
@@ -1,5 +1,8 @@
 import serial
 import time
+import socket
+import codecs
+import binascii
 
 def send_data(serial_port, data):
     try:
@@ -21,16 +24,32 @@ def send_data(serial_port, data):
 
 # 设置串口号
 serial_port = 'COM19' 
+udp_host = '192.168.0.2'
+udp_port = 8080
 
 input_value = [0xF1,0xF2,0xf3,0xf4,0x2a,0x29,0x28,0x27,0x26,0x24,0x23,0x22,0x21,
                0x20,0x31,0x30,0x41,0x42,0x40,0x61,0x60,0x77,0x77,0x70,
                0x7f,0x7f,0x7f,0x70,0x87,0x87,0x87,0x80]
+
+with open('../FPGA/ethernet_character_1.txt', 'r', encoding = 'UTF-8') as ecfile:
+    hex_string = ecfile.read().strip()
+
+ # 创建UDP套接字
+udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+#formatted_string = ''.join(['\\x{}'.format(hex_string[i:i+2]) for i in range(0, len(hex_string), 2)])
+#byte_data = bytearray.fromhex(''.join(hex_string.split()))
+
+byte_data = bytearray.fromhex(hex_string)
+print(' '.join(hex(b) for b in byte_data))
+
+udp_socket.sendto(byte_data, (udp_host, udp_port))
+udp_socket.close()
+print(f"成功发送UDP数据")
+
 
 # 设置要发送的8位数据
 for i in range(32):
     data_to_send = input_value[i]
     send_data(serial_port, data_to_send)
     time.sleep(3)
-
-# 发送数据
-
